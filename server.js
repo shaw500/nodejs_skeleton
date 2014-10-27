7940a03af2f77c79abd12c83788b78bbf90d7086
Matthew Shaw <shaw500@gmail.com>
2014-10-27 04:01:58 +0000

    Ensured default env is corrected logged

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ require('./routes')(app);
 require('./after_routing')(app);
 
 var server = app.listen(3000, function() {
-  console.log('Running in env: ' + process.env.NODE_ENV);
+  console.log('Running in env: ' + (process.env.NODE_ENV || 'dev'));
   console.log('Listening on port %d', server.address().port);
 });
